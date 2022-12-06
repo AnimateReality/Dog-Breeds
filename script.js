6f788a0bc00f4742005777faff15fcfcfc735341
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-07 00:26:54 +1300

    Better dark mode button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = 'coral';
+        body.style.background = '#5bbef7';
         body.style.color = 'black';
         body.style.transition = '2s';
     }else{
