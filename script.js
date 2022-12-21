b5e7e076d92336fdc4c88cdd2ffbe89117d3a330
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-21 13:56:39 +1300

    Table Script
    Table now populated by script that appends cells based on the content of the breeds array

--- script.js
+++ script.js
@@ -13,4 +13,13 @@ toggle.addEventListener('click', function(){
         body.style.transition = '2s';
     }
 });
+var breeds = ["Affenpinscher", "African", "Airedale", "Akita", "Appenzeller", "Australian", "Basenji", "Beagle", "Bluetick", "Borzoi", "Boxer", "Brabancon", "Briard", "Norwegian Buhund", "Staffordshire Bullterrier", "Bulldog", "Bullmastiff", "Cairn", "Canaan Dog", "Chesapeake Bay Retriever", "Chihuahua", "Chinese Crested", "Chow", "Clumber", "Cocker Spaniel", "Collie", "Curly-Coated Retriever", "Dachshund", "Dalmatian", "Dandie Dinmont", "Doberman Pinscher", "English Foxhound", "English Setter", "English Springer Spaniel", "Entlebucher", "Eskimo Dog", "Field Spaniel", "Finnish Lapphund", "Finnish Spitz", "Flat-Coated Retriever", "French Bulldog", "German Pinscher", "German Shepherd", "German Shorthaired Pointer", "German Wirehaired Pointer", "Giant Schnauzer", "Glen of Imaal Terrier", "Golden Retriever", "Gordon Setter", "Great Dane", "Great Pyrenees", "Greater Swiss Mountain Dog", "Greyhound", "Harrier", "Havanese", "Ibizan Hound", "Irish Setter", "Irish Terrier", "Irish Water Spaniel", "Irish Wolfhound", "Italian Greyhound", "Japanese Chin", "Keeshond", "Kerry Blue Terrier", "Komondor", "Kuvasz", "Labrador Retriever", "Lakeland Terrier", "Lhasa Apso", "Lowchen", "Maltese", "Manchester Terrier", "Mastiff", "Mexican Hairless", "Miniature Pinscher", "Miniature Schnauzer", "Newfoundland", "Norfolk Terrier", "Norwegian Elkhound", "Norwich Terrier", "Old English Sheepdog", "Otterhound", "Papillon", "Pekingese", "Pembroke Welsh Corgi", "Petit Basset Griffon Vendeen", "Pharaoh Hound", "Pointer", "Polish Lowland Sheepdog", "Pomeranian", "Poodle", "Portuguese Water Dog", "Pug", "Puli", "Rhodesian Ridgeback", "Rottweiler", "Saint Bernard", "Saluki", "Samoyed", "Schipperke", "Scottish Deerhound", "Scottish Terrier", "Sealyham Terrier", "Shetland Sheepdog", "Shih Tzu", "Siberian Husky", "Silky Terrier", "Skye Terrier", "Smooth Fox Terrier"]
 
+var table = document.getElementsByClassName("container")[0];
+for (var i = 0; i < breeds.length; i++) {
+    var row = document.createElement("tr");
+    var cell = document.createElement("td");
+    cell.innerHTML = breeds[i];
+    row.appendChild(cell);
+    table.appendChild(row);
+  }
