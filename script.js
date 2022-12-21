2f9e749b9252922efbaa0c18b83952fa577478c4
AnimateReality <ronaldchittinappilly@gmail.com>
2022-12-21 13:42:10 +1300

    enable answers with capitals

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
+    this.classList.toggle('bi-brightness-high-fill');
+    if(this.classList.toggle('bi-moon')){
         body.style.background = '#5bbef7';
         body.style.color = 'black';
         body.style.transition = '2s';
